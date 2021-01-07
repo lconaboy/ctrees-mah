adf654bed69939188437fdd629302a1a566b898f
Luke Conaboy <lukeconaboy@gmail.com>
2021-01-07 10:26:00 +0000

    Now plots the binned and binned normalised MAHs

--- py/plot_mah.py
+++ py/plot_mah.py
@@ -1,8 +1,8 @@
-"""
-Really nasty script to plot the output of mah.c.
+"""Really nasty script to plot the output of mah.c.
 
 TODO
    - errorbars
+
 """
 
 import numpy as np
@@ -44,13 +44,27 @@ def get_bins(x):
 
 
 def bin_mah(x):
+    """Bins the mass accretion histories, x, in the format returned by
+    read_mah.  Calculates the binned MAH in whole decades (i.e. 10^5
+    -> 10^6 Msol/h) binned by the final halo mass and the normalised
+    (i.e. M(z)/M(z_f)) MAH binned in the same way.
 
+    :param x: (array) masses in format returned by read_mah
+
+    :returns: binned MAH, error on binned MAH, binned normalised MAH,
+              error on binned normalised MAH, bin edges
+
+    :rtype: (array, array, array, array, array)
+    """
     bin_edges = get_bins(x)
     nbins = len(bin_edges) - 1
 
     # Preallocate arrays for mean and standard deviation
     avg = np.zeros((nbins, x.shape[1]))
     std = np.zeros((nbins, x.shape[1]))
+
+    norm_avg = np.zeros((nbins, x.shape[1]))
+    norm_std = np.zeros((nbins, x.shape[1]))
 
     for i in range(nbins):
         # Get a mask containing the indices of the data in each bin
@@ -59,13 +73,15 @@ def bin_mah(x):
 
         # Get the actual data in the mask
         tmp = x[mask, :]
-
-        # Normlise to final mass
-        tmp = norm_mvir(tmp)
-        
         avg[i, :] = np.nanmean(tmp, axis=0)
         std[i, :] = np.nanstd(tmp, axis=0)
 
+        # Normlise to final mass
+        tmp = norm_mvir(tmp)
+        norm_avg[i, :] = np.nanmean(tmp, axis=0)
+        norm_std[i, :] = np.nanstd(tmp, axis=0)
+
+        
         # Check distribution of mvir vals
         # for i in range(x.shape[1]):
         #     mi, ma = nan_minmax(x[mask, i])
@@ -76,7 +92,7 @@ def bin_mah(x):
         #     plt.xscale('log')
         #     plt.show()
     
-    return avg, std, bin_edges
+    return avg, std, norm_avg, norm_std, bin_edges
 
 
 def bin_norm_mah(x):
@@ -92,7 +108,7 @@ def norm_mvir(x):
     
     for i in range(x.shape[0]):
         y[i, :] = x[i, :] / x[i, 0]
-    
+        print('final halo mass: {0:.2e} Msol/h'.format(x[i, 0]))
     return y
 
 
@@ -137,7 +153,7 @@ def read_mah(fn='out.txt'):
                 # z[i, j] = 1./aexp[i, j] - 1.  # slow, could do this outside loop
                 j += 1
 
-    z = 1./aexp - 1.
+    z = (1. / aexp) - 1.
     # Some scale factors would be NaN, so we can get all of the
     # redshifts by using unique on z -- unique also handily sorts the
     # array.
@@ -149,18 +165,33 @@ def read_mah(fn='out.txt'):
 z, all_z, mvir = read_mah()
 # norm_mvir = norm_mvir(mvir)
 
-fig, ax = plt.subplots(figsize=(6, 6))
-ax.loglog(all_z, mvir)
-ax.set_xlabel('z')
-ax.set_ylabel('M (M$_\odot$/h)')
-ax.set_yscale('log')
-fig.savefig('./out.pdf')
+# fig, ax = plt.subplots(figsize=(6, 6))
+# ax.loglog(all_z, mvir)
+# ax.set_xlabel('z')
+# ax.set_ylabel('M (M$_\odot$/h)')
+# ax.set_yscale('log')
+# fig.savefig('./out.pdf')
 
-# print(nan_minmax(mvir))
-
-nbins = 5
-avg, std, bin_edges = bin_mah(mvir)
+avg, std, norm_avg, norm_std, bin_edges = bin_mah(mvir)
+nbins = avg.shape[0]
+nz = avg.shape[1]
 c = get_cols(nbins)
+
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.set_xlabel('z')
+# ax.set_ylabel('M (M$_\odot$/h)')
+ax.set_ylabel('M(z) (M$_\odot$/h)')
+ax.set_yscale('log')
+ax.set_xscale('log')
+for i in range(nbins):
+    l = '{0:.2e} $\leq$ M (M$_\odot$/h) $<$ {1:.2e}'.format(bin_edges[i],
+                                                            bin_edges[i+1])
+    ax.plot(all_z[0:nz], avg[i, :], c=c[i], label=l)
+    # ax.errorbar(all_z[0:avg.shape[1]], avg[i, :], yerr=std[i, :],
+    #             c=c[i], label=l)
+ax.legend()
+fig.tight_layout()
+fig.savefig('out_binned.pdf', bbox_inches='tight')
 
 fig, ax = plt.subplots(figsize=(6, 6))
 ax.set_xlabel('z')
@@ -168,11 +199,12 @@ ax.set_xlabel('z')
 ax.set_ylabel('M(z)/M({0:.2f})'.format(all_z[0]))
 ax.set_yscale('log')
 ax.set_xscale('log')
-for i in range(avg.shape[0]):
-    l = '{0:.2e} $\leq$ M (M$_\odot$/h) $<$ {1:.2e}'.format(bin_edges[i], bin_edges[i+1])
-    ax.plot(all_z[0:avg.shape[1]], avg[i, :], c=c[i], label=l)
-    # ax.errorbar(all_z[0:avg.shape[1]], avg[i, :], yerr=std[i, :],
+for i in range(nbins):
+    l = '{0:.2e} $\leq$ M (M$_\odot$/h) $<$ {1:.2e}'.format(bin_edges[i],
+                                                            bin_edges[i+1])
+    ax.plot(all_z[0:nz], norm_avg[i, :], c=c[i], label=l)
+    # ax.errorbar(all_z[0:avg.shape[1]], norm_avg[i, :], yerr=norm_std[i, :],
     #             c=c[i], label=l)
 ax.legend()
 fig.tight_layout()
-fig.savefig('out_binned.pdf', bbox_inches='tight')
+fig.savefig('out_norm_binned.pdf', bbox_inches='tight')
